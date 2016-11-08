6be203069cc6340e225004c68a8a474e89234af7
Pierre Laurac <pierre.laurac@gmail.com>
2016-11-07 16:43:35 -0800

    renaming README

--- iOSEngineeringTest/FA_Message.swift
+++ iOSEngineeringTest/FA_Message.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+class FA_Message {
+  
+  init(dict: Dictionary<String,Any>) {
+    self.content_loaded = dict["content_loaded"] as! Bool
+    self.blurb = dict["blurb"] as! String
+    self.body = dict["body"] as! String
+  }
+  
+  var content_loaded: Bool!
+  var blurb: String!
+  var body: String!
+}
--- iOSEngineeringTest/FA_TableViewController.swift
+++ iOSEngineeringTest/FA_TableViewController.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+
+class FA_TableViewController: UITableViewController {
+  
+  fileprivate let messageCellIdentifier = "messageCell"
+  
+  fileprivate var messages: [FA_Message] = []
+  
+  override func viewDidLoad() {
+    getConversation()
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return messages.count
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: messageCellIdentifier, for: indexPath)
+    cell.textLabel?.text = "Message \(indexPath.row)"
+    return cell
+  }
+  
+  
+  fileprivate func getConversation() {
+    do {
+      let pathToComposer = Bundle.main.path(forResource: "conversation", ofType: "json")
+      let jsonData = try NSData(contentsOfFile: pathToComposer!) as Data
+      let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as! Dictionary<String, Any>
+      let rawMessages = json["messages"] as! Array<Dictionary<String, Any>>
+      rawMessages.forEach({ (message) in
+        self.messages.append(FA_Message(dict: message))
+      })
+    } catch {}
+  }
+}
